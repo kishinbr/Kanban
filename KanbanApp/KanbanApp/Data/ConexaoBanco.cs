bc505eb2b16d4a60d7c86e2b5be4a2cd1658c0cc
kishinbr <viniciuskishibr@gmail.com>
2026-08-26 22:51:11 -0300

    feat: adicionando banco e classe usuario

---
+++ KanbanApp/KanbanApp/Data/ConexaoBanco.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace KanbanApp.Data
+{
+    public class ConexaoBanco
+    {
+        private readonly string _stringConexao;
+
+        public ConexaoBanco(IConfiguration configuration)
+        {
+            _stringConexao = configuration.GetConnectionString("KanbanDb")
+                ?? throw new Exception("String de conexão 'KanbanDb' não encontrada no appsettings.json");
+        }
+
+        public IDbConnection CriarConexao()
+        {
+            return new NpgsqlConnection(_stringConexao);
+        }
+    }
+}
--- KanbanApp/KanbanApp/Program.cs
+++ KanbanApp/KanbanApp/Program.cs
@@ -1,7 +1,11 @@
+using KanbanApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ConexaoBanco>();
+
 
 var app = builder.Build();
 
@@ -12,6 +16,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
